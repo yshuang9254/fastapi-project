bd71ce2d8ed7f2bed6062bd34af59204d0e07eba
黃宥姍 <Y.S.Huang9254@gmail.com>
2025-08-25 01:08:38 +0800

    修正登入失敗回傳的狀態碼，從 404 改為 401

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,8 +3,6 @@ from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from .. import database,schemas,models,utils,oauth2
 
-
-
 router = APIRouter(tags = ["Authentication"])
 
 @router.post("/login",response_model = schemas.Token)
@@ -12,9 +10,9 @@ def login(user_credentials:OAuth2PasswordRequestForm = Depends(),db:Session = De
     email = user_credentials.username.lower()
     user = db.query(models.User).filter(models.User.email == email).first()
     if not user:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail = "Invalid Credentials")
+        raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,detail = "Invalid Credentials")
     if not utils.verify(user_credentials.password,user.password):
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,detail = "Invalid Credentials")
+        raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED,detail = "Invalid Credentials")
     access_token = oauth2.create_access_token(data = {"user_id":user.id})
     return {"access_token":access_token,"token_type":"bearer"}
 
